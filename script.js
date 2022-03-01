4e5eaf23f9050048c3a6b4d946721d7db03ecd38
SeryiBaran <seryibaranminepy@yandex.ru>
2022-03-01 21:49:09 +0300

    Formatting with Prettier

--- script.js
+++ script.js
@@ -3,13 +3,15 @@
 function Check() {
     let plus = "Elliot";
     let answer = confirm("fuck society?", "");
-    let plusanswer = prompt("Как звали главного героя Mr.Robot (Имя на английском с большой буквы)", "");
+    let plusanswer = prompt(
+        "Как звали главного героя Mr.Robot (Имя на английском с большой буквы)",
+        ""
+    );
 
     if (plusanswer == plus && answer == true) {
-        document.getElementById("cr").textContent = "Молодец, правильный ответ - мой телеграм @12313131";
-    }
-    else {
+        document.getElementById("cr").textContent =
+            "Молодец, правильный ответ - мой телеграм @12313131";
+    } else {
         return alert("Неверно, ты не заслужил быть частью f5ociety");
     }
 }
-
