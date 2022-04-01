0f8ddbf690b4578b98dd65616bc81d8518e98467
Erghel <88831850+Erghel@users.noreply.github.com>
2022-04-01 14:30:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Check() {
     let plusanswer = prompt("Как звали главного героя Mr.Robot (Имя на английском с большой буквы)", "");
 
     if (plusanswer == plus && answer == true) {
-        document.getElementById("cr").textContent = "Молодец, правильный ответ - мой телеграм @12313131";
+        document.getElementById("cr").textContent = "Молодец, правильный ответ. Напиши нам на почту - fuckfucksociety@protonmail.com. Теперь ты часть нашей команды";
     }
     else {
         return alert("Неверно, ты не заслужил быть частью f5ociety");
